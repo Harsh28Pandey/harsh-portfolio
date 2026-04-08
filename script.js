b28a2e1057883c18632125b80a9309e7f0b61ea2
Harsh28Pandey <harsh28.knp@gmail.com>
2026-04-08 11:00:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ arrowRight.addEventListener('click', () => {
     }
     else {
         // when we add a new project then update this index when we upload 12 projects the index value should be 11 (projects-1)
-        index = 17;
+        index = 19;
         arrowRight.classList.add('disabled');
     }
 
